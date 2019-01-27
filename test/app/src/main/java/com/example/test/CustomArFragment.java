9eb12bbd9dd5cb6003dab23df0426dc34e524ce2
Yifei Zhang <yifei7.zhang@gmail.com>
2019-01-26 22:44:33 -0500

    finished phase 2 of dineAR; can now use refeerence image to detect a plane, and imposes 3D model on top of it

--- test/app/src/main/java/com/example/test/CustomArFragment.java
+++ test/app/src/main/java/com/example/test/CustomArFragment.java
@@ -1,20 +1,29 @@
 package com.example.test;
 
 import android.util.Log;
+
 import com.google.ar.core.Config;
 import com.google.ar.core.Session;
 import com.google.ar.sceneform.ux.ArFragment;
+
+import java.io.IOException;
 
 public class CustomArFragment extends ArFragment {
     @Override
     protected Config getSessionConfiguration(Session session) {
         getPlaneDiscoveryController().setInstructionView(null);
+        //instead of waving phone around to find plane, now we want to find plane based off of an image
         Config config = new Config(session);
         config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
         session.configure(config);
         getArSceneView().setupSession(session);
-
+        if ((((MainActivity) getActivity()).setupAugmentedImagesDb(config, session))) {
+            Log.d("SetupAugImgDb", "Success");
+        } else {
+            Log.e("SetupAugImgDb","Faliure setting up db");
+        }
         return config;
     }
+
 
 }
--- test/app/src/main/java/com/example/test/MainActivity.java
+++ test/app/src/main/java/com/example/test/MainActivity.java
@@ -1,104 +1,110 @@
 package com.example.test;
 
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.content.Context;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.MotionEvent;
 import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
 import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.AugmentedImageDatabase;
+import com.google.ar.core.Config;
 import com.google.ar.core.Frame;
-import com.google.ar.core.HitResult;
-import com.google.ar.core.Plane;
 import com.google.ar.core.Session;
 import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity {
 
-
-//    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-//            Log.e(TAG, "Sceneform requires Android N or later");
-//            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
-//            activity.finish();
-//            return false;
-//        }
-//        String openGlVersionString =
-//                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
-//                        .getDeviceConfigurationInfo()
-//                        .getGlEsVersion();
-//        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
-//            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
-//            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
-//                    .show();
-//            activity.finish();
-//            return false;
-//        }
-//        return true;
-//    }
-
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private static final double MIN_OPENGL_VERSION = 3.0;
-
-
     ArFragment arFragment;
-    ModelRenderable hamburgerRenderable;
+    boolean shouldAddModel = true;
+
 
     @Override
-    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
-
-
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (!checkIsSupportedDeviceOrFinish(this)) {
-//            return;
-//        }
+
         setContentView(R.layout.activity_main);
-        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
 
-        ModelRenderable.builder()
-                .setSource(this, Uri.parse("yukari.sfb"))
-                .build()
-                .thenAccept(renderable -> hamburgerRenderable = renderable)
-                .exceptionally(throwable -> {
-                    Toast toast =
-                            Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.CENTER, 0, 0);
-                    toast.show();
-                    return null;
-                });
+        arFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
+        arFragment.getPlaneDiscoveryController().hide();
+        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
 
-
-        arFragment.setOnTapArPlaneListener(
-                (HitResult hitresult, Plane plane, MotionEvent motionevent) -> {
-                    if (hamburgerRenderable == null){
-                        return;
-                    }
-
-                    Anchor anchor = hitresult.createAnchor();
-                    AnchorNode anchorNode = new AnchorNode(anchor);
-                    anchorNode.setParent(arFragment.getArSceneView().getScene());
-
-                    TransformableNode burger = new TransformableNode(arFragment.getTransformationSystem());
-                    burger.setParent(anchorNode);
-                    burger.setRenderable(hamburgerRenderable);
-                    burger.select();
-                }
-        );
     }
 
+
+    private void placeObject(ArFragment arFragment, Anchor anchor, Uri uri) {
+        ModelRenderable.builder()
+                .setSource(arFragment.getContext(), uri)
+                .build()
+                .thenAccept(modelRenderable -> addNodeToScene(arFragment, anchor, modelRenderable))
+                .exceptionally(throwable -> {
+                            Toast.makeText(arFragment.getContext(), "Error:" + throwable.getMessage(), Toast.LENGTH_LONG).show();
+                            return null;
+                        }
+                );
+    }
+
+    private void onUpdateFrame(FrameTime frameTime) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        Collection<AugmentedImage> augmentedImages = frame.getUpdatedTrackables(AugmentedImage.class);
+        for (AugmentedImage augmentedImage : augmentedImages) {
+            if (augmentedImage.getTrackingState() == TrackingState.TRACKING) {
+                if (augmentedImage.getIndex() == 0 && shouldAddModel) {
+                    placeObject(arFragment, augmentedImage.createAnchor(augmentedImage.getCenterPose()), Uri.parse("Hamburger.sfb"));
+                    shouldAddModel = false;
+                }
+            }
+        }
+    }
+
+    public boolean setupAugmentedImagesDb(Config config, Session session) {
+
+        try {
+            InputStream inputStream = getAssets().open("imgdatabase.imgdb");
+            AugmentedImageDatabase augmentedImageDatabase = AugmentedImageDatabase.deserialize(session, inputStream);
+            config.setAugmentedImageDatabase(augmentedImageDatabase);
+            session.configure(config);
+            return true;
+        }
+        catch (IOException e){
+            return false;
+        }
+//        Bitmap bitmap = loadAugmentedImage();
+//        if (bitmap == null) {
+//            return false;
+//        }
+//        augmentedImageDatabase.addImage("cat", bitmap);
+    }
+
+//    private Bitmap loadAugmentedImage() {
+//        try (InputStream is = getAssets().open("cat.jpg")) {
+//            return BitmapFactory.decodeStream(is);
+//        } catch (IOException e) {
+//            Log.e("ImageLoad", "IO Exception", e);
+//        }
+//        return null;
+//    }
+
+    private void addNodeToScene(ArFragment arFragment, Anchor anchor, Renderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+    }
 }
